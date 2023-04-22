fc7d9daddc2f2f40e4646010ac0127a6d3e14ce3
KevinOrozcoMC <korozco01@manhattan.edu>
2023-04-22 16:34:55 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame as pg
+import sys
+from settings import *
+
+from map import *
+from player import *
+from raycasting import *
+
+
+class Game:
+    def __init__(self):
+        # render screen and clock for frame rate
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        # get delta of time
+        self.delta_time = 1
+        self.new_game()
+
+    def new_game(self):
+        # instance of map class
+        self.map = Map(self)
+        # instance of player
+        self.player = Player(self)
+        # instance of ray casting method
+        self.raycasting = RayCasting(self)
+
+    # update screen and display information
+    def update(self):
+        self.player.update()
+        self.raycasting.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        # make a black screen
+        self.screen.fill('black')
+        # draw map
+        # self.map.draw()
+        # draw player
+        # self.player.draw()
+
+
+    # event handler
+    def check_events(self):
+        for event in pg.event.get():
+            # if player hits the X at top right or presses escape key exit the game
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    # game loop calling update and draw methods
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ player.py
@@ -0,0 +1,78 @@
+import pygame as pg
+import math
+
+from settings import *
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+
+    def movement(self):
+        # calculate movement given angle and position
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+
+        # to make movement speed independent of frame rate, you need delta time value for each frame
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        # bind keys to movement for player controls
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w]:
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            dx += -speed_sin
+            dy += speed_cos
+
+        self.check_wall_collision(dx, dy)
+
+        # rotate controls (?)
+        if keys[pg.K_LEFT]:
+            self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        if keys[pg.K_RIGHT]:
+            self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau  # tau = 2*pi
+
+    # adding collision
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+
+    # draw player
+    def draw(self):
+        # line of direction of movement
+        # pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+        #             (self.x * 100 + WIDTH * math.cos(self.angle),
+        #              self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        # player
+        pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
+
+    def update(self):
+        self.movement()
+
+    # returns coordinates
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    # returns coordinates in map to know which tile you're on
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
